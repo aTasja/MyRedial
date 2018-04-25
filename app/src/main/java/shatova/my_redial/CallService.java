44160ee30dcf538302767ecdaec2a243e5f9629b
Anastasia Shatova <a_tasja@mail.ru>
2018-04-25 19:06:10 +0300

    Need to abandon the CallLog in CallService, because it is impossible immediately to receive the duration of the completed call.

--- app/src/main/java/shatova/my_redial/CallService.java
+++ app/src/main/java/shatova/my_redial/CallService.java
@@ -2,27 +2,59 @@ package shatova.my_redial;
 
 import android.Manifest;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.CallLog;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class CallService extends Service {
 
     String TAG = "myLOG";
-    String mLastState;
+    int mLastState;
+
+    int attempts;
+    String number;
+    int tryCall;
+    boolean nextCall;
+    boolean havePermission;
+    private android.content.BroadcastReceiver mReceiver;
+
 
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "SERVICE on Create");
+
+
+
+        // Initialize a new BroadcastReceiver instance for local intents from ServiceReceiver.
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // Display a notification that radio has been connected.
+                makeCall();
+            }
+        };
+
+        // Register Local Broadcast receiver - use to receive messages from service
+        IntentFilter serviceFilter = new IntentFilter("possibleToMakeNextCall");
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, serviceFilter);
+
     }
 
     /**
@@ -43,58 +75,90 @@ public class CallService extends Service {
                               int flags,
                               int startid) {
 
+
+        havePermission = (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED);
+
         Bundle extras = intent.getExtras();
-        if (extras != null && (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED)){
-            String number = intent.getStringExtra("number");
-            Log.d(TAG, "call intent received RINGING to " + number);
-            getApplicationContext().startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + number)));
+        if (extras != null){
+            number = intent.getStringExtra("number");
+            attempts = intent.getIntExtra("attempts", 0);
+            Log.d(TAG, "SRVICE intent received NUMBER  = " + number + " ATTEMPTS = " + attempts);
+            tryCall = 1;
+            makeCall();
+
         }
 
-        /*
-         * Use PhoneStateListener instances for listening to phone state
-         */
-/*
-        TelephonyManager mIncomingCallsReceiver = (TelephonyManager)getApplicationContext()
-                .getSystemService(Context.TELEPHONY_SERVICE);
-        if (mIncomingCallsReceiver != null) {
-            mIncomingCallsReceiver.listen(new PhoneStateListener() {
-                @Override
-                public void onCallStateChanged(int state, String incomingNumber) {
-                    //Log.d(TAG, "PHONE STATE RADIO broadcast received = " + state);
-                    switch (state) {
-                        case TelephonyManager.CALL_STATE_IDLE:
-                            if (state != mLastState) {
-                                Log.d(TAG, "phone listener = " + "IDLE");
-                                if (mLastState == TelephonyManager.CALL_STATE_OFFHOOK) {
-                                    nextCall = true;
-                                    Log.d(TAG, "phone listener NEXT CALL = " + nextCall);
-                                }
-                                callStatus = "IDLE";
-                            }
-                            break;
+        //if (havePermission && tryCall == 1){
 
-                        case TelephonyManager.CALL_STATE_OFFHOOK:
-                            if (state != mLastState) {
-                                callStatus = "OFFHOOK";
-                                nextCall = false;
-                                Log.d(TAG, "phone listener = " + callStatus);
-                            }
-                            break;
-
-            /*case TelephonyManager.CALL_STATE_RINGING:
-                if (state != mLastState){
-                    Log.d(TAG, "phone listener = " + "RINGING");
-                    callStatus = "RINGING";}
-                break;
-                    } mLastState = state;
-                }
+        //}
 
 
-            }, PhoneStateListener.LISTEN_CALL_STATE);
-        }
-*/
+
         // Restart Service if it shuts down.
         return START_STICKY;
+    }
+
+    private void makeCall(){
+        int lastCallDuration = Integer.parseInt(getCallDetails(getApplicationContext()));
+        Log.d(TAG, "Длительность последнего звонка = " + lastCallDuration);
+        if  (lastCallDuration == 0) {
+            if (tryCall <= attempts) {
+                Log.d(TAG, "ЗВОНЮ в " + tryCall + " раз из " + attempts);
+                try {
+                    getApplicationContext().startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + number)));
+                    tryCall++;
+                } catch (SecurityException sEx) {
+                    Log.d(TAG, "Security permission == " + sEx);
+                }
+            } else {
+                Log.d(TAG, "SERVICE ПОПЫТКИ закончились");
+                onDestroy();
+            }
+        }else {
+            Log.d(TAG, "SERVICE Длительность последнего звонка больше 0");
+        }
+
+    }
+
+    public static String getCallDetails(Context context) {
+
+
+        // StringBuilder sb = new StringBuilder();
+        if (ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
+
+
+            Cursor managedCursor = context.getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, null);
+            //int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+            int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+            //int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+            int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
+            //sb.append("Call Details :");
+            if (managedCursor.moveToPosition(0)) {
+                managedCursor.moveToNext();
+                managedCursor.moveToPosition(0);
+                //String phNumber = managedCursor.getString(number);
+                String callType = managedCursor.getString(type);
+                //String callDate = managedCursor.getString(date);
+                //Date callDayTime = new Date(Long.valueOf(callDate));
+                String callDuration = managedCursor.getString(duration);
+                //String dir = null;
+                int dircode = Integer.parseInt(callType);
+                switch (dircode) {
+                    case CallLog.Calls.OUTGOING_TYPE:
+                        //Log.d(TAG, "OUTGOING");
+                        return callDuration;
+
+                }
+            }managedCursor.close();
+        }
+
+        return "";
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "SERVICE onDestroy");
     }
 }
